be7a49ba4bd07b2197d4b450ccec97014ea400fd
gerswin <g3rswin@gmail.com>
2013-03-12 03:09:03 -0430

    gf

--- php/mongo.php
+++ php/mongo.php
@@ -2,15 +2,12 @@
 date_default_timezone_set("America/Caracas"); 	
 function mongo($api, $url, $numero, $mensaje, $status)
 	{
-	$services_json = json_decode(getenv("VCAP_SERVICES") , true);
-	$mongo_config = $services_json["mongodb-1.8"][0]["credentials"];
-	$username = $mongo_config["username"];
-	$password = $mongo_config["password"];
-	$hostname = $mongo_config["hostname"];
-	$port = $mongo_config["port"];
-	$db = $mongo_config["db"];
-	$name = $mongo_config["name"];
-	$connect = "mongodb://${username}:${password}@${hostname}:${port}/${db}";
+	$host   = $_ENV["OPENSHIFT_MONGODB_DB_HOST"];
+     $user   = $_ENV["OPENSHIFT_MONGODB_DB_USERNAME"];
+     $passwd = $_ENV["OPENSHIFT_MONGODB_DB_PASSWORD"];
+     $port   = $_ENV["OPENSHIFT_MONGODB_DB_PORT"];    
+	$db = "api";
+	$connect = "mongodb://" . $user . ":" . $passwd . "@" . $host . ":" . $port;
 	$m = new Mongo($connect);
 	$db = $m->selectDB($db); 
 	$modo = "sms";
@@ -28,22 +25,19 @@ function mongo($api, $url, $numero, $mensaje, $status)
 	$collection->insert($obj);
 	}
 	
-mongo ("testapi","343242343432","041256589","ola ke ase como va todo2","delivered");
+echo mongo ("testapi","343242343432","041256589","ola ke ase como va todo2","delivered");
+echo "algo";
 
-
-$services_json = json_decode(getenv("VCAP_SERVICES") , true);
-$mongo_config = $services_json["mongodb-1.8"][0]["credentials"];
-$username = $mongo_config["username"];
-$password = $mongo_config["password"];
-$hostname = $mongo_config["hostname"];
-$port = $mongo_config["port"];
-$db = $mongo_config["db"];
-$name = $mongo_config["name"];
-$connect = "mongodb://${username}:${password}@${hostname}:${port}/${db}";
-$m = new Mongo($connect);
-$db = $m->selectDB($db);
-$modo = "sms";
-$collection = $db->smsout;
+$host   = $_ENV["OPENSHIFT_MONGODB_DB_HOST"];
+     $user   = $_ENV["OPENSHIFT_MONGODB_DB_USERNAME"];
+     $passwd = $_ENV["OPENSHIFT_MONGODB_DB_PASSWORD"];
+     $port   = $_ENV["OPENSHIFT_MONGODB_DB_PORT"];    
+	$db = "api";
+	$connect = "mongodb://" . $user . ":" . $passwd . "@" . $host . ":" . $port;
+	$m = new Mongo($connect);
+	$db = $m->selectDB($db); 
+	$modo = "sms";
+	$collection = $db->smsout;
 $cursor = $collection->find()->limit(2);
 
 foreach($cursor as $obj)
