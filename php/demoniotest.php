0b77ca16e6089c8e70e0cff38984759d80152248
gerswin <g3rswin@gmail.com>
2013-03-14 22:48:20 -0430

    Signed-off-by: gerswin <g3rswin@gmail.com>

--- php/demoniotest.php
+++ php/demoniotest.php
@@ -88,7 +88,7 @@ function android($numero, $txt, $id)
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $server . $str);	
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 20);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
 	$retorno = curl_exec($ch);
 	curl_close($ch);
 	return $retorno;
